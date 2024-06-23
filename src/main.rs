c25e092990273d2502e99929f20656ed0af1d470
monodyle <monodylh@gmail.com>
2024-06-23 17:39:23 +0700

    wip: watch

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use elden_ring_death_counter::{read_save_file, Character};
+use notify::{
+    event::{AccessKind, AccessMode},
+    Error, Event, EventKind, Watcher,
+};
 use std::{env, fs, path::Path};
 
 fn write_output(
@@ -36,18 +40,41 @@ fn main() {
         .expect("unknown command, try: 'debug', 'get', or 'watch'");
     let input_file = env::args().nth(2).expect("no save file provided");
 
-    let buffer = fs::read(input_file).expect("unable to open save file");
+    let buffer = fs::read(input_file.clone()).expect("unable to open save file");
     let result = read_save_file(buffer);
 
     let output_folder = env::args().nth(3).unwrap_or("output".to_owned());
-    let output_format = env::args().nth(4).unwrap_or("Death: {{}}".to_owned());
+    let output_format = env::args().nth(4).unwrap_or("Death: {}".to_owned());
+
+    let write = move || {
+        write_output(&result, &output_format, &output_folder, false)
+            .expect("unable to write output")
+    };
+
     match command.as_ref() {
-        "debug" => write_output(&result, &output_format, &output_folder, true)
-            .expect("unable to write output"),
-        "get" => write_output(&result, &output_format, &output_folder, false)
-            .expect("unable to write output"),
-        "watch" => write_output(&result, &output_format, &output_folder, false)
-            .expect("unable to write output"),
+        "debug" => write(),
+        "get" => write(),
+        "watch" => {
+            write();
+
+            let mut watcher =
+                notify::recommended_watcher(move |res: Result<Event, Error>| match res {
+                    Ok(event) => {
+						println!("event: {:?}", event);
+                        if event.kind == EventKind::Access(AccessKind::Close(AccessMode::Write)) {
+                            write()
+                        }
+                    }
+                    Err(e) => println!("watch error: {:?}", e),
+                })
+                .expect("unable to create a watcher");
+
+            loop {
+                watcher
+                    .watch(Path::new(&input_file), notify::RecursiveMode::Recursive)
+                    .expect("unable to watch input file");
+            }
+        }
         _ => panic!("invalid command '{command}', try again with: 'debug', 'get', or 'watch'"),
     }
 }
